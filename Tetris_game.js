5c5d9f4c6bf11a7bc35f7d283cb271d003f910d0
Mohammad <mohammad.alabed132@gmail.com>
2023-02-26 23:50:43 +0400

    Added Text + Some edits

--- Tetris_game.js
+++ Tetris_game.js
@@ -12,6 +12,10 @@ let GameBoard_arr_height = 20;
 let GameBoard_arr_width = 12;
 let StartX = 4;
 let StartY= 0;
+let score = 0;
+let level = 1;
+let winOrLose = 'Playing';
+
 let coordinate_arr = [...Array(GameBoard_arr_height)].map(e => Array(GameBoard_arr_width).fill(0));
 let current_piece = [[1,0], [0,1], [1,1], [2,1]];
 
@@ -20,6 +24,7 @@ let Pieces_Color = ['blue', 'lightblue', 'red', 'yellow', 'green', 'orange', 'cy
 let curr_Pieces_Color;
 
 let GameBoard_arr = [...Array(GameBoard_arr_height)].map(e => Array(GameBoard_arr_width).fill(0));
+let stoppedBoard_arr = [...Array(GameBoard_arr_height)].map(e => Array(GameBoard_arr_width).fill(0));
 
 let Direction = {
     Idle:0,
@@ -38,6 +43,8 @@ class Coordinates {
 
 }
 
+
+
 document.addEventListener("DOMContentLoaded", SetupCanvas);
 
 function SetupCanvas(){
@@ -50,14 +57,34 @@ function SetupCanvas(){
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.strokeStyle = "black";
     ctx.strokeRect(8, 8, 280, 462);
+    ctx.fillStyle = 'black';
+    ctx.font = '21px Arial';
+    ctx.fillText("SCORE", 300, 98);
+
+    ctx.strokeRect(300, 107, 161, 24);
+
+    ctx.fillText(score.toString(), 310, 127);
+    ctx.fillText("LEVEL", 300, 157);
+    ctx.strokeRect(300, 171, 161, 24);
+
+    ctx.fillText("WIN / LOSE", 300, 221);
+    ctx.fillText(winOrLose, 310, 261);
+    ctx.fillText("CONTROLS", 300, 354);
+    ctx.strokeRect(300, 366, 161, 104);
+    ctx.font = '19px Arial';
+    ctx.fillText("A : Move Left", 310, 388);
+    ctx.fillText("D : Move Right", 310, 413);
+    ctx.fillText("S : Move Down", 310, 438);
+    ctx.fillText("E : Rotate Right", 310, 463);
     document.addEventListener("keydown", OnkeyPress);
     Create_Pieces();
     Create_Piece();
 
     CreateCoordArr();
     DrawPiece()
-};
 
+
+}
 
 function OnkeyPress(key){
     if (key.keyCode === 65) {
@@ -83,7 +110,7 @@ function OnkeyPress(key){
         StartY++;
         DrawPiece();
     }
-};
+}
 
 function CreateCoordArr() {
     let i=0, j=0;
